c817eb06520e19e6155e9c0adb185610a8c1bbc3
KaiShen <kaishen@ant-internet.com>
2022-06-03 17:30:26 +0800

    modified server.js

--- app/src/index.js
+++ app/src/index.js
@@ -1,36 +1,45 @@
-const puppeteer = require('puppeteer');
-const request = require('request-promise-native');
-const getContainerIP = require('./util/getContainerIP');
+const fs = require("fs");
+const path = require("path");
+const puppeteer = require("puppeteer");
 
+const isProduction = process.env.NODE_ENV === "production";
+const mockPng = fs
+	.readFileSync(path.join(__dirname, "mock.png"))
+	.toString("base64");
 (async () => {
-  // Workaround for https://github.com/GoogleChrome/puppeteer/issues/2242
-  const chrome = await getContainerIP('chrome');
+	const browser = isProduction
+		? await puppeteer.connect({ browserWSEndpoint: "ws://127.0.0.1:3000" })
+		: await puppeteer.launch({
+				devtools: true,
+				headless: false,
+		  });
+	const page = await browser.newPage();
+	await Promise.all([
+		page._client.send("Console.disable"),
+		page._client.send("Page.setBypassCSP", { enabled: true }),
+	]);
+	await page._client.send("Network.setBlockedURLs", {
+		urls: ["ga-audiences"],
+	});
 
-  const options = {
-    uri: `http://${chrome}:9222/json/version`,
-    json: true,
-    resolveWithFullResponse: true
-  };
+	await page._client.send("Fetch.enable", {
+		patterns: [
+			{
+				resourceType: "Image",
+				requestStage: "Request",
+			},
+		],
+	});
+	page._client.on("Fetch.requestPaused", async (e) => {
+		page._client.send("Fetch.fulfillRequest", {
+			requestId: e.requestId,
+			responseCode: 200,
+			body: mockPng,
+		});
+	});
 
-  request(options)
-    .then((res) => {
-      let webSocket = res.body.webSocketDebuggerUrl;
-      console.log(`WebsocketUrl: ${webSocket}`);
-
-      (async () => {
-        try {
-          const browser = await puppeteer.connect({browserWSEndpoint: webSocket});
-          const page    = await browser.newPage();
-
-          page.setJavaScriptEnabled(true);
-          await page.goto(`https://www.google.com`, { waitUntil: 'networkidle0' });
-        }
-        catch(e) {
-          console.log(e);
-        }
-      })();
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+	await page.goto("https://www.trendyol.com", { waitUntil: "networkidle2" });
+	const pageContents = await page.content();
+	if (isProduction) await browser.disconnect();
+	console.log(pageContents);
 })();
